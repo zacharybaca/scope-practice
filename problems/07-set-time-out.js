86b188a59f9a38517cad178713b2f1046eb35b9f
Zachary Baca <zachary.baca@msn.com>
2023-07-27 10:07:17 -0500

    completed setTimeOut

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -20,6 +20,14 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 // Your code here
 
+function funcTimer(time, func) {
+    setTimeout(func, time);
+}
+
+funcTimer(5000, () => {
+    console.log('Hello')
+})
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
