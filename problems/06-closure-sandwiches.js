3e2cd7bfc82101fbfca66721f2d146c3e8fb4599
zacharybaca <zachary.baca@msn.com>
2023-07-27 17:07:16 -0500

    completed more problems

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -19,6 +19,27 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 
 // Your code here
 
+function sandwichMaker() {
+  let str = "One sandwich with tomato";
+
+   return (ingredient) => {
+     str += " and " + ingredient;
+     return str;
+  }
+}
+
+let sandwich = sandwichMaker(); // => returns a function
+
+console.log(sandwich("spinach")) // => "One sandwich with tomato and spinach"
+console.log(sandwich("jelly")) // => "One sandwich with tomato and spinach and jelly"
+console.log(sandwich("bread")) // => "One sandwich with tomato and spinach and jelly and bread"
+
+
+//Another Sandwich:
+let sandwich2 = sandwichMaker(); // => returns a function
+
+console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
